ef5a12758f44a20fdb62f6acc487e7e36add4615
Nagao <241314@st.yoshida-g.ac.jp>
2025-01-16 09:58:12 +0900

    ポップアップのヘッダーにコメント追加

--- Damagepop.h
+++ Damagepop.h
@@ -1,7 +1,16 @@
+//===================================
+//
+//ダメージ表示関連のヘッダー
+//Author;長尾悠成
+//
+//===================================
+
+// 重複確認とインクルード
 #ifndef _DAMEGE_H_
 #define _DAMEGE_H_
 #include "Main.h"
 
+// 処理の色々
 void InitDamege(void);
 void UninitDamege(void);
 void UpdateDamege(void);
